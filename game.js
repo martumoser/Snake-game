3dfb102b7ddecd265e3bf3083ca0d3dd3eaed8ad
Martina Moser <mmoser.ar@gmail.com>
2020-11-05 15:17:07 -0300

    Completed item 01.02

--- game.js
+++ game.js
@@ -1,12 +1,40 @@
-var canvas = null,
-ctx = null;
-function paint(ctx) {
-    ctx.fillStyle = '#0f0';
-    ctx.fillRect(50, 50, 100, 60);
-}
-function init() {
+ var canvas = null,
+    ctx = null;
+    x = 50,
+    y = 50;
+
+ window.requestAnimationFrame = (function(){
+     return window.requestAnimationFrame ||
+     window.webkitRequestAnimationFrame ||
+     function (callback) {
+         window.setTimeout(callback, 17);
+     };    
+ }());
+
+ function paint(ctx) {
+    ctx.fillStyle = '#D2CCD3';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#EB09F6';
+    ctx.fillRect(x, y, 10, 10);
+ }
+
+ function act(){
+    x += 2;
+    if (x > canvas.width) {
+        x = 0;
+    }
+ }
+
+ function run() {
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
+ }
+
+ function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    paint(ctx);
-}
-window.addEventListener('load', init, false);
+    run();
+ }
+
+ window.addEventListener('load', init, false);
